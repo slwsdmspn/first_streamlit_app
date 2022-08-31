d74a19519ffab98d9d9d5f533d858b5b2e4fe604
SLWSDM SPN <112551808+slwsdmspn@users.noreply.github.com>
2022-08-31 06:50:43 -0400

    Update streamlit_app.py
    Added title and menu items texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
-import streamlit;
+import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinash & Rocket Smoothie')
+sreeamlt.text('Hard-Boilde Free-Range Egg')
