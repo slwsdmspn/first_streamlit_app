2393b66d802d4449d2b6cc3b1adb2affaa32831c
SLWSDM SPN <112551808+slwsdmspn@users.noreply.github.com>
2022-08-31 06:57:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinash & Rocket Smoothie')
-streamlit.text('Hard-Boilde Free-Range Egg')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣 Kale, Spinash & Rocket Smoothie')
+streamlit.text('🥑 Hard-Boilde Free-Range Egg')
+streamlit.text('🍞 Avocado Toast')
